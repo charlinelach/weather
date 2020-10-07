a4d5d2a63c356a07186bd838e092621a431d1217
Charline <charlinerlach@gmail.com>
2020-10-06 21:13:44 -0500

    Visual and informational updates to the cards, search field, and jumbotron

--- script.js
+++ script.js
@@ -1,35 +1,62 @@
 // API: 9a14accd96c70fa4fba63f1bdc2c6c06
 
-var city = $("citySearch").val()
+$("#searchBtn").click(function () {
+    var searchValue = $("#citySearch").val();
 
-$("#searchBtn").click(function() {
-    localStorage.setItem("pastBtn");
+    // Currrent
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&units=imperial&appid=9a14accd96c70fa4fba63f1bdc2c6c06", function (data) {
+        console.log(data);
+
+        var city = data.name;
+        var date = new Date();
+        var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        var temp = "Temperature: " + data.main.temp + "°F";
+        var humidity = "Humidity: " + data.main.humidity + "%";
+        var windSpeed = "Wind Speed: " + data.wind.speed + " mph";
+
+        $('.city').append(city);
+        document.getElementById("date").innerHTML = date.toDateString();
+        $('.icon').attr('src', icon);
+        $('.temp').append(temp);
+        $('.humidity').append(humidity);
+        $('.windSpeed').append(windSpeed);
+
+        var latitude = data.coord.lat;
+        var longitude = data.coord.lon;
+
+        $.getJSON("http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=9a14accd96c70fa4fba63f1bdc2c6c06", function (data) {
+            console.log(data);
+            var uvDisplay = "UV Index: " + data.value;
+            
+            $('.uvDisplay').addClass("lowUV");
+            $('.uvDisplay').append(uvDisplay);
+        });
+    });
+
+
+    // Forecasting
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchValue + "&cnt=5&units=imperial&appid=9a14accd96c70fa4fba63f1bdc2c6c06", function (data) {
+        console.log(data);
+
+        var date = data.list[0].dt_txt;
+        var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+        var temp = "Temperature: " + data.list[0].main.temp + "°F";
+        var humidity = "Humidity: " + data.list[0].main.humidity + "%";
+
+
+        // Day One
+        var date = data.list[1].dt_txt;
+        var icon = "http://openweathermap.org/img/w/" + data.list[1].weather[0].icon + ".png";
+        var temp = "Temperature: " + data.list[1].main.temp + "°F";
+        var humidity = "Humidity: " + data.list[1].main.humidity + "%";
+
+        $('.dateOne').append(date);
+        $('.iconOne').attr('src', icon);
+        $('.tempOne').append(temp);
+        $('.humidityOne').append(humidity);
+
+    });
+
 });
 
-$.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=9a14accd96c70fa4fba63f1bdc2c6c06", function (data) {
-    console.log(data);
-
-    var city = data.city.name;
-    var date = data.list[0].dt_txt;
-    var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
-    var temp = "Temperature: " + data.list[0].main.temp + "°F";
-    var humidity = "Humidity: " + data.list[0].main.humidity + "%";
-    var windSpeed = "Wind Speed: " + data.list[0].wind.speed + " mph";
-
-    $('.city').append(city);
-    $('.date').append(date);
-    $('.icon').attr('src', icon);
-    $('.temp').append(temp);
-    $('.humidity').append(humidity);
-    $('.windSpeed').append(windSpeed);
-
-});
-
-$.getJSON("http://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + data.city[2.0] + "&lon=" + data.city[2.1] + "&appid=9a14accd96c70fa4fba63f1bdc2c6c06", function (data) {
-    console.log(data);
-
-    var uvDisplay = "UV Index: " + data.value;
-
-    $('.uvDisplay').append(uvDisplay);
-
-});
+//dayFive
